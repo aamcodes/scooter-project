37cbd4e26f895f0468e271a7247afadf29c1e076
aamcodes <aaronmendezcodes@gmail.com>
2023-02-24 20:26:12 -0500

    removed type checking

--- src/User.js
+++ src/User.js
@@ -1,8 +1,6 @@
 class User {
 	// User code here
 	constructor(username, password, age, loggedIn = false) {
-		// Type Validations
-		validateUser(username, password, age);
 		this.username = username;
 		this.password = password;
 		this.age = age;
@@ -20,14 +18,6 @@ class User {
 	logout() {
 		this.loggedIn = false;
 	}
-}
-
-function validateUser(username, password, age) {
-	if (typeof username !== 'string')
-		throw new Error('Username must be a string');
-	if (typeof password !== 'string')
-		throw new Error('Password must be a string');
-	if (typeof age !== 'number') throw new Error('Age must be a number');
 }
 
 module.exports = User;
